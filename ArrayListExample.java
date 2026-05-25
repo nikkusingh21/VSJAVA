beae6104fae69d6562f3ac8c937841fd1737a14b
nikkusingh21 <niksachoudhary98@gmail.com>
2026-05-25 08:27:13 +0530

    Add files via upload

---
+++ ArrayListExample.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+class ArrayListExample {
+    public static void main(String[] args) {
+
+        ArrayList<String> list = new ArrayList<>();
+
+        list.add("Hello");
+        list.add("World");
+        list.add("Java");
+        list.add("Programming");
+
+        System.out.println("ArrayList: " + list);
+    }
+}
